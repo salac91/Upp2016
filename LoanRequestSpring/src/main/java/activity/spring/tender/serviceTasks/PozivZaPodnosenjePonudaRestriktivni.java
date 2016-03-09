841f86b62210425aae10eb86a81968c0c4d6c2d2
JovanPopov <jovan.popov.88@gmail.com>
2016-03-09 16:25:49 +0100

    od danas

--- LoanRequestSpring/src/main/java/activity/spring/tender/serviceTasks/PozivZaPodnosenjePonudaRestriktivni.java
+++ LoanRequestSpring/src/main/java/activity/spring/tender/serviceTasks/PozivZaPodnosenjePonudaRestriktivni.java
@@ -1,5 +1,15 @@
 package activity.spring.tender.serviceTasks;
 
-public class PozivZaPodnosenjePonudaRestriktivni {
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class PozivZaPodnosenjePonudaRestriktivni implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execute) throws Exception {
+		// TODO Auto-generated method stub
+		Boolean tajmeristekao=false;
+		execute.setVariable("tajmeristekao", tajmeristekao);
+	}
 
 }
--- LoanRequestSpring/src/main/java/activity/spring/tender/serviceTasks/PripremaOglasa.java
+++ LoanRequestSpring/src/main/java/activity/spring/tender/serviceTasks/PripremaOglasa.java
@@ -3,13 +3,16 @@ package activity.spring.tender.serviceTasks;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
-public class PozivZaPodnosenjePonuda implements JavaDelegate{
+public class PripremaOglasa implements JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution execute) throws Exception {
 		// TODO Auto-generated method stub
-		Boolean tajmeristekao=false;
-		execute.setVariable("tajmeristekao", tajmeristekao);
+		
+		Boolean tajmeristekaorest=false;
+		execute.setVariable("tajmeristekaorest", tajmeristekaorest);
+		
 	}
+	
 
 }
